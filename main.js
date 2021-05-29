054b85c42c27d802accd87e8f0f5cc0d4635389f
NormatjonDev <qozimurodovnormatjon@gmail.com>
2021-05-29 14:23:23 +0500

    praktika
    shunchaki praktika uchun joylashtirildi

---
+++ main.js
@@ -0,0 +1,33 @@
+// let name = {
+//     name: 'normat', yosh: 28, daraja: 'magistr', uylangan: false}
+//
+// console.log(name)
+//2-dars
+// let arr=['normat', 'abror', 'sardor']
+// console.log(arr)
+// console.log(arr[2])
+//
+// arr[2]='aziz'
+// console.log(arr[2])
+// prompt( 'siz talabamisiz')
+
+    /*3 dars*/
+// for (let i=0; i<10; i++){
+//     console.log(i)
+// }
+// let test=[]
+// let nat=[
+//     'sening isming nima',
+//     'qayerda tugilgansan',
+//     'yoshing nechida'
+// ]
+// for (let i=0; i<nat.length; i++){
+//     test[i]=nat[i]
+// }
+// console.log(test)
+/* 4 dars if else*/
+//5 dars
+function myFunction(name){
+    console.log(`salom mening ismim ${name}`)
+}
+myFunction( 'samar')
